dbd0ceeb98c07589e07dc09dbdb114a9191e3124
renzjohnpedrozo <renzjohnbagasinpedrozo@gmail.com>
2025-09-19 23:21:58 +0800

    Add Dockerfile and composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1b87bbc60090e4f59e3cf649e4d7ff130f564a77',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1b87bbc60090e4f59e3cf649e4d7ff130f564a77',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
