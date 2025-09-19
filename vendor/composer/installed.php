3c905653b5dbbf12e3dc60cea3af9d3c143d541c
Renz John Pedrozo <renzjohnbagasinpedrozo@gmail.com>
2025-09-19 10:49:23 +0800

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '610d0cafa8c52734e9cacf5f83b7cf44a5e40074',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '610d0cafa8c52734e9cacf5f83b7cf44a5e40074',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
