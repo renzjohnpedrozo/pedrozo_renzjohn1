629557b5185b7a9d456d4bdcdc5fbac2cba2e423
renzjohnpedrozo <renzjohnbagasinpedrozo@gmail.com>
2025-09-20 23:01:36 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST'),
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> getenv('DB_USER'),
-    'password'	=> getenv('DB_PASS'),
-    'database'	=> getenv('DB_NAME'),
+    'username'	=> 'sql12799392',
+    'password'	=> 'RpzTJN3qTY',
+    'database'	=> 'sql12799392',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
